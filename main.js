f4181a1c4e94b709a4b640c75aeafbb087b44a4e
Brian Cavalier <brian@hovercraftstudios.com>
2014-03-05 15:47:15 -0500

    Add templated list example

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+var fluent = require('wire/config/fluent');
 var TodosController = require('./TodosController');
 
-module.exports = {
-	todosController: new TodosController(),
-	todosModel: []
-};
+var listTemplate = require('./list.html');
+
+module.exports = fluent(function(config) {
+	return config
+		.add('todos@view', ['render', 'insert', 'qs'], function(render, insert, qs) {
+			var view = render(listTemplate);
+			qs('section').appendChild(view);
+			return view;
+		})
+		.add('todos@controller', TodosController)
+		.add('todos@model', function() {
+			return [];
+		});
+});
+
+//module.exports = {
+//	todosController: new TodosController(),
+//	todosModel: []
+//};
